d266d314d2e83ca49e7965d92a8aaf688ac36a1f
Matthew Monroe <matthewryan.monroe@gmail.com>
2022-05-04 23:12:41 -0400

    finalized boilerplate with build webpack

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import Phaser from 'phaser';
+import SceneA from './scenes/SceneA.js';
+import version from '../package.json';
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  backgroundColor: '#000000',
+  parent: 'thegame',
+  scene: [SceneA],
+  title: 'The Game',
+  version,
+  banner: {
+    text: '#ffffff',
+    background: ['#fff200', '#38f0e8', '#00bff3', '#ec008c'],
+    hidePhaser: true,
+  },
+};
+
+// eslint-disable-next-line no-unused-vars
+const game = new Phaser.Game(config);
